9c64a6c039177aa963fb080ab7aebec555c14a0a
Veeru <immaculateempyrean@gmail.com>
2021-08-30 15:23:46 -0700

    refactored FilterRowTags as a seperate component and allowed color to be specified to it as a string whenever required.

---
+++ src/components/primitive/FilterRowTag.vue
@@ -0,0 +1,27 @@
+<template>
+    <span class="tag is-medium">
+        {{tagName}}
+        <button @click="$emit('tag:deleted',tagName)" class="delete is-small"></button>
+    </span>
+</template>
+
+<script>
+export default {
+    name: "FilterRowTag",
+    props:{
+        tagName:{
+            type: String,
+            default: "NoName"
+        },
+        tagColor:{
+            type: String,
+            default: "#000"
+        }
+    },
+    mounted(){
+        //add the specified color to the tag when Successfully Started Up..
+        this.$el.style.backgroundColor=this.tagColor;
+        this.$el.style.color="#fff";
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="home-view">
+
+    <!-- start creating the main tool-bar -->
     <div id="main-toolbar">
+      <!-- the main menu component is not expected to play a major role in version-1 -->
       <MainMenu />
 
+      <!-- version 1 has only two functional buttons positioned prominently on the main-toolbar -->
       <div class="functional-buttons">
         <Button 
           id="SourceManagerButton"
@@ -13,29 +17,15 @@
           class="button is-primary is-large"
           buttonText="Filters"/>
       </div>
+
+      <!-- the search bar is responsible for accepting substring to match against files.. -->
       <SearchBar />
     </div>
+    <!-- ended creating the main tool-bar -->
 
-    <div id="filters-row">
-      <span class="filter-tags">
-          Filters Added -
-          <span class="tag is-warning is-medium">
-          Test
-          <button class="delete is-small"></button>
-          </span>
-          <span class="tag is-link is-medium">
-          Test2
-          <button class="delete is-small"></button>
-          </span>
-          <span class="tag is-danger is-medium">
-          Test3
-          <button class="delete is-small"></button>
-          </span>
-          <span class="tag is-success is-medium">
-          Test4
-          <button class="delete is-small"></button>
-          </span>
-      </span>
+    <div id="filters-row">  
+      Filters Added -
+      <FilterRowTag />
     </div>
     <!-- these two modals are to be shown only when the corresponding buttons are pressed -->
     <SourceManager v-if="showSourceModal"/>
@@ -51,6 +41,7 @@ import FilterManager  from '../components/FilterManager.vue';
 import SourceManager  from '../components/SourceManager.vue';
 
 import Button         from '../components/primitive/Button.vue';
+import FilterRowTag   from '../components/primitive/FilterRowTag.vue';
 
 export default {
   name: 'Home',
@@ -59,7 +50,8 @@ export default {
     SearchBar,
     FilterManager,
     SourceManager,
-    Button
+    Button,
+    FilterRowTag
   },
   data (){
     return {
@@ -80,8 +72,6 @@ export default {
     justify-content: space-between;
     align-items: center;
 
-    //border-bottom: 2px solid black;
-
     #MainMenu{
       flex: 0 0 15%;
     }
@@ -100,13 +90,11 @@ export default {
   }
 
   #filters-row{
+    display: flex;
+    align-items: center;
+    column-gap: $spacing-small;
+
     padding: $spacing-small $spacing-normal;
     border-bottom: 2px solid black;
-
-    .filter-tags{
-      display: flex;
-      column-gap: $spacing-small;
-      align-items: center;
-    }
   }
 </style>
