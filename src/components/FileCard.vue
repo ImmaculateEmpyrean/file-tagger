d8685444bfe1cf9d05fa6137193cb2974b2edc5d
Veeru <immaculateempyrean@gmail.com>
2021-08-31 09:46:17 -0700

    made more progress creating file cards.

--- src/components/FileCard.vue
+++ src/components/FileCard.vue
@@ -1,4 +1,7 @@
 <template>
+<div class="card-container">
+
+
     <div class="card">
         <header class="card-header">
            <p class="card-header-title last-modified">
@@ -8,8 +11,9 @@
         <div class="card-content">
             <div class="media">
                 <div class="media-left">
-                    <figure class="image is-48x48">
-                        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                    <figure class="image is-48x48 main-icon">
+                        <image class="fas fa-file"></image>
+                        <!-- <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"> -->
                     </figure>
                 </div>
                 <div class="media-content path-size">
@@ -17,13 +21,26 @@
                     <p class="subtitle is-6">{{size}}</p>
                 </div>
             </div>
+            <div class="content tags-container">
+                <Tag />
+                <Tag />
+                <Tag />
+                <Tag />
+            </div>
         </div>
+    </div>
+
     </div>
 </template>
 
 <script>
+import Tag from '../components/primitive/Tag.vue';
+
 export default {
     name: "FileCard",
+    components:{
+        Tag
+    },
     props:{
         lastModified:{
             type: String,
@@ -37,12 +54,46 @@ export default {
             type: String,
             default: "500 MB"
         }
+    },
+    methods:{
+        setCardWidth(){
+            let cardContainer = this.$el;
+            if(window.innerWidth > 1000){
+                cardContainer.style.minWidth = "1000px";
+                cardContainer.style.maxWidth = "1000px";
+            }  else {
+                cardContainer.style.minWidth = `${window.innerWidth}px`
+                cardContainer.style.maxWidth = `${window.innerWidth}px`
+            }
+        }
+    },
+    mounted(){
+        this.setCardWidth();
+        window.addEventListener('resize',this.setCardWidth);
     }
 }
 </script>
 
 <style lang="scss" scoped>
+    @import '../assets/scss/setting.scss';
+
     .last-modified{
         justify-content: flex-end;
+    }
+
+    .card-container{
+        max-width: 1000px;
+        //min-width: 1000px;
+        margin: 0 auto;
+    }
+
+    .tags-container{
+        display: flex;
+        column-gap: $spacing-small;
+    }
+
+    figure.main-icon > * {
+        width: 100%;
+        height: 100%;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+require("@fortawesome/fontawesome-free/js/brands.js");
+require("@fortawesome/fontawesome-free/js/solid.js");
+require("@fortawesome/fontawesome-free/js/fontawesome.js");
+
 createApp(App).use(router).mount('#app')
