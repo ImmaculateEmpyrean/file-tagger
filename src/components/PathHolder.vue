21a5ccc5ee24faceff42500475ad3ece4fb0ebe6
Veeru <immaculateempyrean@gmail.com>
2021-09-01 08:00:41 -0700

    started fixing the source control.

--- src/components/PathHolder.vue
+++ src/components/PathHolder.vue
@@ -1,5 +1,19 @@
 <template>
-    <h1>path holder</h1>
+    <div class="field has-addons">
+        <div class="control">
+            <input class="input" type="text" placeholder="SourcePath">
+        </div>
+        <div class="control">
+            <a class="button is-link">
+                <i class="fas fa-edit"></i>
+            </a>
+        </div>
+        <div class="control">
+            <a class="button is-danger">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,3 +21,7 @@ export default {
     name: "PathHolder"
 }
 </script>
+
+<style lang="scss" scoped>
+    @import 'bulma/bulma.sass';
+</style>
--- src/components/SourceManager.vue
+++ src/components/SourceManager.vue
@@ -8,10 +8,10 @@
                         <button class="delete" aria-label="close"></button>
                     </header>
                     <section class="modal-card-body">
-                        <p class="title is-5 is-spaced">
-                            Source paths can be added and edited or deleted at will here.
-                        </p>
+                        <p class="title is-5 is-spaced">Source paths can be added and edited or deleted at will here.</p>
                         <p class="subtitle is-6">If a file has to be tracked by this application, then it must be inside one of the source directories listed below..</p>
+
+                        <Button id="new-source-button" class="button is-small is-link" buttonText="New"/>
 
                         <!-- input componnets are to be added here in runtime as I dont Know How Many To Add At Run Time -->
                         <PathHolder />
@@ -26,12 +26,22 @@
 </template>
 
 <script>
+import Button from './primitive/Button.vue';
 import PathHolder from './PathHolder.vue';
 
 export default {
     name: "SourceManager",
     components:{
-        PathHolder
+        PathHolder,
+        Button
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    @import '../assets/scss/setting.scss';
+
+    #new-source-button{
+        margin-bottom: $spacing-normal;
+    }
+</style>
